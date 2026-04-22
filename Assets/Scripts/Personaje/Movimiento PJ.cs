1a4ac53c8276e5256952ac2f3cf098eff84d6067
Alvaro <alvaromedici75@gmail.com>
2026-04-21 22:51:26 -0300

    Movimiento Pj y Camara

--- Assets/Scripts/Personaje/Movimiento PJ.cs
+++ Assets/Scripts/Personaje/Movimiento PJ.cs
@@ -1,16 +1,67 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] float gravity = -9.81f;
+    [SerializeField] float moveSpeed;
+    [SerializeField] float sensitivity;
+    [SerializeField] float minLimit = -80f;
+    [SerializeField] float maxLimit = 80f;
+    [SerializeField] Transform cameraTransform;
+
+
+    [SerializeField] Movimiento inputAction;
+    [SerializeField] CharacterController characterController;
+    private Vector2 move;
+    private Vector2 look;
+    private Vector2 speed;
+    private float currentRotationY;
+
+    private void Awake()
     {
-        
+        inputAction = new Movimiento();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        inputAction.Player.Enable();
+        inputAction.Player.Move.performed += SetMovement;
+        inputAction.Player.Move.canceled += obj => move = Vector2.zero;
+        inputAction.Player.Look.performed += SetLook;
+        inputAction.Player.Look.canceled += obj => look = Vector2.zero;
+    }
+
+    private void SetLook(InputAction.CallbackContext context)
+    {
+        look = context.ReadValue<Vector2>();
+    }    
+    private void SetMovement(InputAction.CallbackContext context)
+    {
+        move = context.ReadValue<Vector2>();
+    }
+
+    private void Update()
+    {
+        Movement();
+        Look();
+    }
+
+    private void Look()
+    {
+        Vector2 mouseDelta = look * sensitivity;
+        currentRotationY = Mathf.Clamp(currentRotationY - mouseDelta.y, minLimit, maxLimit);
+        cameraTransform.localRotation = Quaternion.Euler(currentRotationY, 0f, 0f); 
+        transform.Rotate(Vector3.up * mouseDelta.x);
+    }
+    private void Movement()
+    {
+        Vector3 move = transform.right * this.move.x + transform.forward * this.move.y;
+        characterController.Move(move * moveSpeed * Time.deltaTime);
+
+        speed.y += gravity * Time.deltaTime;
+        characterController.Move(speed * Time.deltaTime);
     }
 }
